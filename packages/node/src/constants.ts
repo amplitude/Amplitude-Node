a6c386b34a9905dc0b6d1f5fbcad2dc868d82a9e
haoliu <hao.liu@amplitude.com>
2020-05-20 23:17:23 -0700

    Fix incorrect library version

--- packages/node/src/constants.ts
+++ packages/node/src/constants.ts
@@ -1,3 +1,3 @@
 export const SDK_NAME = 'amplitude-node';
-export const SDK_VERSION = '0.2.0';
+export const SDK_VERSION = '0.3.1';
 export const AMPLITUDE_SERVER_URL = 'https://api.amplitude.com/2/httpapi';
