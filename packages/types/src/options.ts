d0e64bfbbbd31ada0b2931056b4caa135f6c4f8d
Kelvin Lu <kellu1997@gmail.com>
2020-08-31 17:35:40 -0700

    stricter options

--- packages/types/src/options.ts
+++ packages/types/src/options.ts
@@ -7,12 +7,12 @@ export interface Options {
   /**
    * Whether you opt out from sending events.
    */
-  optOut?: boolean;
+  optOut: boolean;
 
   /**
    * @deprecated Set it if you are using proxy server.
    */
-  serverUrl?: string;
+  serverUrl: string;
 
   /**
    * Configuration of the logging verbosity of the SDK.
@@ -21,12 +21,12 @@ export interface Options {
    * 2 = WARN: Warnings will be generated around dangerous/deprecated features.
    * 3 = VERBOSE: All SDK actions will be logged.
    */
-  logLevel?: LogLevel;
+  logLevel: LogLevel;
 
   /**
    * Whether or not the SDK should be started in debug mode.
    * This will enable the SDK to generate logs at WARN level or above, if the
    * logLevel is not specified.
    */
-  debug?: boolean;
+  debug: boolean;
 }
