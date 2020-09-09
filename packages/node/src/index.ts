8cab913358b6eb140dc6ab56f499c9bed9dbac1c
Kelvin Lu <kellu1997@gmail.com>
2020-09-08 22:57:36 -0700

    export the retry and transport classes

--- packages/node/src/index.ts
+++ packages/node/src/index.ts
@@ -1,3 +1,5 @@
 export { Event, Options, Response, Status } from '@amplitude/types';
 export { NodeClient } from './nodeClient';
+export { RetryHandler } from './retryHandler';
 export { init } from './sdk';
+export { HTTPTransport } from './transports';
