e73adf94e270fafccf6c2281dea9c38d8e9d5663
Kelvin Lu <kellu1997@gmail.com>
2020-09-14 13:40:32 -0700

    restructure logic in tests to catch it one level higher

--- packages/node/test/mocks/retry.ts
+++ packages/node/test/mocks/retry.ts
@@ -1,12 +1,38 @@
 import { RetryHandler } from '../../src/';
+import { Transport, Payload, Status, Response } from '@amplitude/types';
 
 // Reduce the retry limit in favor of faster tests
 export const MOCK_MAX_RETRIES = 3;
 
+export class MockThrottledTransport implements Transport {
+  private _throttledId: string;
+  // The number of payloads that got throttled
+  public throttleCount: number = 0;
+  // The number of payloads that did not get throttled
+  public unthrottleCount: number = 0;
+  public constructor(throttledId: string) {
+    this._throttledId = throttledId;
+  }
+
+  public sendPayload(payload: Payload): Promise<Response> {
+    const isMatch = payload.events.some(event => event.user_id === this._throttledId);
+    if (isMatch) {
+      this.throttleCount += 1;
+      return Promise.resolve({ status: Status.RateLimit, statusCode: 429 });
+    } else {
+      this.unthrottleCount += 1;
+      return Promise.resolve({ status: Status.Success, statusCode: 200 });
+    }
+  }
+}
+
 export class TestRetry extends RetryHandler {
   public retryCount: Map<string, Map<string, number>> = new Map<string, Map<string, number>>();
 
-  public constructor() {
-    super('NOT_A_REAL_API_KEY', { maxRetries: MOCK_MAX_RETRIES });
+  public constructor(transport: Transport) {
+    super('NOT_A_REAL_API_KEY', {
+      maxRetries: MOCK_MAX_RETRIES,
+      transportClass: transport,
+    });
   }
 }
--- packages/node/test/retry.test.ts
+++ packages/node/test/retry.test.ts
@@ -1,4 +1,4 @@
-import { TestRetry, MOCK_MAX_RETRIES } from './mocks/retry';
+import { TestRetry, MOCK_MAX_RETRIES, MockThrottledTransport } from './mocks/retry';
 import * as nock from 'nock';
 import { Event, Status } from '@amplitude/types';
 import { asyncSleep } from '@amplitude/utils';
@@ -16,95 +16,64 @@ const generateEvent = (userId: string): Event => {
   };
 };
 
+const setupRetry = () => {
+  const transport = new MockThrottledTransport(FAILING_USER_ID);
+  const retry = new TestRetry(transport);
+  return {
+    retry,
+    transport,
+  };
+};
+
 describe('retry mechanisms layer', () => {
   // A helper that persistently listens to nock and returns the # of
   // times a user id has been included and hasn't been included.
-  let nockHook = { matchPassCount: 0, matchFailCount: 0 };
+
+  let { transport, retry } = setupRetry();
   beforeEach(() => {
-    // Responses with FAILING_USER_ID should fail
-    nock(AMPLITUDE_SERVER_URL)
-      .persist()
-      .post(
-        () => true,
-        body => {
-          const events: Array<Event> = body.events ?? [];
-          const isMatch = events.some(event => event.user_id === FAILING_USER_ID);
-
-          if (isMatch) {
-            nockHook.matchPassCount += 1;
-            return true;
-          } else {
-            nockHook.matchFailCount += 1;
-            return false;
-          }
-        },
-      )
-      .reply(400);
-
-    // Responses without FAILING_USER_ID should return successfully
-    nock(AMPLITUDE_SERVER_URL)
-      .persist()
-      .post(
-        () => true,
-        body => {
-          const events: Array<Event> = body.events ?? [];
-          return !events.some(event => event.user_id === FAILING_USER_ID);
-        },
-      )
-      .reply(200);
-  });
-  // After each test, remove any remaining mocks and reset the nock hook.
-  afterEach(() => {
-    nock.cleanAll();
-    nockHook = {
-      matchPassCount: 0,
-      matchFailCount: 0,
-    };
+    // create new instances before each test
+    const newObjects = setupRetry();
+    transport = newObjects.transport;
+    retry = newObjects.retry;
   });
 
   it('should not retry events that pass', async () => {
     const payload = [generateEvent(PASSING_USER_ID)];
 
-    const retryHandler = new TestRetry();
-
-    const response = await retryHandler.sendEventsWithRetry(payload);
+    const response = await retry.sendEventsWithRetry(payload);
 
     expect(response.status).toBe(Status.Success);
     expect(response.statusCode).toBe(200);
     // One response goes out matching the initial send
-    expect(nockHook.matchFailCount).toBe(1);
+    expect(transport.unthrottleCount).toBe(1);
   });
 
   it('should retry events that fail', async () => {
     const payload = [generateEvent(FAILING_USER_ID)];
-
-    const retryHandler = new TestRetry();
-
-    const response = await retryHandler.sendEventsWithRetry(payload);
+    const response = await retry.sendEventsWithRetry(payload);
 
     // Sleep and wait for retries to end
     await asyncSleep(1000);
 
-    expect(response.status).toBe(Status.Invalid);
-    expect(response.statusCode).toBe(400);
+    expect(response.status).toBe(Status.RateLimit);
+    expect(response.statusCode).toBe(429);
     // One response goes out matching the initial send, MOCK_MAX_RETRIES for the retry layer
-    expect(nockHook.matchPassCount).toBe(MOCK_MAX_RETRIES + 1);
+    expect(transport.throttleCount).toBe(MOCK_MAX_RETRIES + 1);
   });
 
   it('will not throttle user ids that are not throttled', async () => {
     const payload = [generateEvent(FAILING_USER_ID), generateEvent(PASSING_USER_ID)];
-    const retryHandler = new TestRetry();
-    const response = await retryHandler.sendEventsWithRetry(payload);
+    const response = await retry.sendEventsWithRetry(payload);
 
     // Sleep and wait for retries to end
     await asyncSleep(1000);
 
     // The initial send should return as a fail
-    expect(response.status).toBe(Status.Invalid);
-    expect(response.statusCode).toBe(400);
+    expect(response.status).toBe(Status.RateLimit);
+    expect(response.statusCode).toBe(429);
     // One response goes out matching the initial send
-    expect(nockHook.matchPassCount).toBe(MOCK_MAX_RETRIES + 1);
+    expect(transport.throttleCount).toBe(MOCK_MAX_RETRIES + 1);
     // One response goes out for the passing event not getting 'throttled'
-    expect(nockHook.matchFailCount).toBe(1);
+    expect(transport.unthrottleCount).toBe(1);
   });
 });
