0243b76674e2db275fa2c401227c283b505865ba
Kelvin Lu <kellu1997@gmail.com>
2020-09-21 22:19:49 -0700

    refactor tests for new types

--- packages/node/test/retry.test.ts
+++ packages/node/test/retry.test.ts
@@ -1,6 +1,6 @@
 import { TestRetry, MOCK_MAX_RETRIES } from './mocks/retry';
 import { MockTransport } from './mocks/transport';
-import { Event, Status, ResponseBody } from '@amplitude/types';
+import { Event, Status, Response } from '@amplitude/types';
 import { asyncSleep } from '@amplitude/utils';
 
 const FAILING_USER_ID = 'data_monster';
@@ -18,7 +18,7 @@ describe('retry mechanisms layer', () => {
   let transport = new MockTransport(FAILING_USER_ID, null);
   let retry = new TestRetry(transport);
 
-  const setupRetry = (body: ResponseBody | null = null) => {
+  const setupRetry = (body: Response | null = null) => {
     transport = new MockTransport(FAILING_USER_ID, body);
     retry = new TestRetry(transport);
   };
@@ -68,14 +68,17 @@ describe('retry mechanisms layer', () => {
   describe('fast-stop mechanisms for payloads', () => {
     it('will not allow a events exceeding daily quota to be retried', async () => {
       const body: ResponseBody = {
-        code: 429,
-        error: 'NOT_A_REAL_ERROR',
-        epsThreshold: 0,
-        throttledEvents: [],
-        throttledDevices: {},
-        throttledUsers: {},
-        exceededDailyQuotaDevices: {},
-        exceededDailyQuotaUsers: { [FAILING_USER_ID]: 100 },
+        status: Status.RateLimit,
+        statusCode: 429,
+        body: {
+          error: 'NOT_A_REAL_ERROR',
+          epsThreshold: 0,
+          throttledEvents: [],
+          throttledDevices: {},
+          throttledUsers: {},
+          exceededDailyQuotaDevices: {},
+          exceededDailyQuotaUsers: { [FAILING_USER_ID]: 100 },
+        },
       };
       setupRetry(body);
 
@@ -87,12 +90,15 @@ describe('retry mechanisms layer', () => {
       expect(transport.failCount).toBe(1);
     });
     it('will not allow a single event that failed to be retried', async () => {
-      const body: ResponseBody = {
-        code: 400,
-        error: 'NOT_A_REAL_ERROR',
-        missingField: null,
-        eventsWithInvalidFields: {},
-        eventsWithMissingFields: {},
+      const body: Response = {
+        status: Status.Invalid,
+        statusCode: 400,
+        body: {
+          error: 'NOT_A_REAL_ERROR',
+          missingField: null,
+          eventsWithInvalidFields: {},
+          eventsWithMissingFields: {},
+        },
       };
       setupRetry(body);
 
@@ -105,13 +111,18 @@ describe('retry mechanisms layer', () => {
     });
 
     it('will not allow events with invalid fields to be retried', async () => {
-      const body: ResponseBody = {
-        code: 400,
-        error: 'NOT_A_REAL_ERROR',
-        missingField: null,
-        eventsWithInvalidFields: { MISSING_EVENT_FIELD: [0, 1] },
-        eventsWithMissingFields: {},
+      const body: Response = {
+        status: Status.Invalid,
+        statusCode: 400,
+        body: {
+          code: 400,
+          error: 'NOT_A_REAL_ERROR',
+          missingField: null,
+          eventsWithInvalidFields: { MISSING_EVENT_FIELD: [0, 1] },
+          eventsWithMissingFields: {},
+        },
       };
+
       setupRetry(body);
 
       const payload = [generateEvent(FAILING_USER_ID), generateEvent(FAILING_USER_ID)];
@@ -123,12 +134,15 @@ describe('retry mechanisms layer', () => {
     });
 
     it('will not allow payloads with invalid fields to be retried', async () => {
-      const body: ResponseBody = {
-        code: 400,
-        error: 'NOT_A_REAL_ERROR',
-        missingField: 'MISSING_PAYLOAD_FIELD',
-        eventsWithInvalidFields: {},
-        eventsWithMissingFields: {},
+      const body: Response = {
+        status: Status.Invalid,
+        statusCode: 400,
+        body: {
+          error: 'NOT_A_REAL_ERROR',
+          missingField: 'MISSING_PAYLOAD_FIELD',
+          eventsWithInvalidFields: {},
+          eventsWithMissingFields: {},
+        },
       };
       setupRetry(body);
 
