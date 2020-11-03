610a5d2b04e6a5b63e53f3f155cac4a9ca0281c9
John Tran <jooohhn@users.noreply.github.com>
2020-11-02 22:11:15 -0800

    feat(utils): Add isNodeEnv, isBrowserEnv, prototypeJsFix (#45)
    * feat: Add prototypeJsFix to utils
    
    * feat: Add isBrowserEnv and prototypeJsFix to exports

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,4 +1,4 @@
 export { generateBase36Id, DEVICE_ID_LENGTH } from './base36';
 export { logger } from './logger';
-export { asyncSleep, getGlobalAmplitudeNamespace } from './misc';
+export { asyncSleep, getGlobalAmplitudeNamespace, isBrowserEnv, isNodeEnv, prototypeJsFix } from './misc';
 export { collectInvalidEventIndices } from './response';
--- packages/utils/src/misc.ts
+++ packages/utils/src/misc.ts
@@ -49,3 +49,27 @@ export const getGlobalAmplitudeNamespace = (): any => {
 export const asyncSleep = (milliseconds: number): Promise<void> => {
   return new Promise(resolve => setTimeout(resolve, milliseconds));
 };
+
+/**
+ * Fixes browser edge case where Prototype.js injects Array.prototype.toJSON and breaks the built-in JSON.stringify()
+ *
+ * @returns true if Array.prototype.toJSON was deleted, false if not
+ */
+export const prototypeJsFix = (): boolean => {
+  // Augment and cast built-ins to represent Prototype.js injection
+  interface Window {
+    Prototype?: Object;
+  }
+  interface ArrayConstructor {
+    prototype?: { toJSON?: Function };
+  }
+  if (isBrowserEnv()) {
+    const augmentedWindow = window as Window;
+    const augmentedArray = Array as ArrayConstructor;
+    if (augmentedWindow.Prototype !== undefined && augmentedArray.prototype?.toJSON !== undefined) {
+      delete augmentedArray.prototype.toJSON;
+      return true;
+    }
+  }
+  return false;
+};
