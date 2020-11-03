bdbeb689d9ac559ebe80f851d239f19ce591125f
Kelvin Lu <kellu1997@gmail.com>
2020-11-03 02:41:23 -0800

    refactor of identify validate and set

--- packages/identify/src/identify.ts
+++ packages/identify/src/identify.ts
@@ -1,5 +1,7 @@
 import { SpecialEventType, IdentifyEvent, IdentifyOperation, IdentifyUserProperties } from '@amplitude/types';
 
+// A special value used by this class when a value is not needed by identify
+// Ex. for unset, we set $unset: { 'example_user_property': '-' }
 export const UNSET_VALUE = '-';
 
 export class Identify {
@@ -14,75 +16,60 @@ export class Identify {
     };
   }
 
-  public set(property: string, value: string | number | Array<string | number>): Identify {
-    if (this._validate(IdentifyOperation.SET, property, value)) {
-      this._safeSet(IdentifyOperation.SET, property, value);
+  public getUserProperties(): IdentifyUserProperties {
+    const userPropertiesCopy: IdentifyUserProperties = {};
+    const fields: Array<IdentifyOperation> = Object.keys(this._userProperties) as any;
+    for (let field of fields) {
+      if (field in this._userProperties) {
+        userPropertiesCopy[field] = { ...this._userProperties[field] };
+      }
     }
 
+    return userPropertiesCopy;
+  }
+
+  public set(property: string, value: string | number | Array<string | number>): Identify {
+    this._safeSet(IdentifyOperation.SET, property, value);
     return this;
   }
 
   public setOnce(property: string, value: string | number | Array<string | number>): Identify {
-    if (this._validate(IdentifyOperation.SET_ONCE, property, value)) {
-      this._safeSet(IdentifyOperation.SET_ONCE, property, value);
-    }
-
+    this._safeSet(IdentifyOperation.SET_ONCE, property, value);
     return this;
   }
 
   public append(property: string, value: string | number | Array<string | number>): Identify {
-    if (this._validate(IdentifyOperation.APPEND, property, value)) {
-      this._safeSet(IdentifyOperation.APPEND, property, value);
-    }
-
+    this._safeSet(IdentifyOperation.APPEND, property, value);
     return this;
   }
 
   public prepend(property: string, value: string | number | Array<string | number>): Identify {
-    if (this._validate(IdentifyOperation.PREPEND, property, value)) {
-      this._safeSet(IdentifyOperation.PREPEND, property, value);
-    }
-
+    this._safeSet(IdentifyOperation.PREPEND, property, value);
     return this;
   }
 
   public postInsert(property: string, value: string | number | Array<string | number>): Identify {
-    if (this._validate(IdentifyOperation.PREINSERT, property, value)) {
-      this._safeSet(IdentifyOperation.PREINSERT, property, value);
-    }
-
+    this._safeSet(IdentifyOperation.PREINSERT, property, value);
     return this;
   }
 
   public preinsert(property: string, value: string | number | Array<string | number>): Identify {
-    if (this._validate(IdentifyOperation.PREINSERT, property, value)) {
-      this._safeSet(IdentifyOperation.PREINSERT, property, value);
-    }
-
+    this._safeSet(IdentifyOperation.PREINSERT, property, value);
     return this;
   }
 
   public remove(property: string, value: string | number | Array<string | number>): Identify {
-    if (this._validate(IdentifyOperation.REMOVE, property, value)) {
-      this._safeSet(IdentifyOperation.REMOVE, property, value);
-    }
-
+    this._safeSet(IdentifyOperation.REMOVE, property, value);
     return this;
   }
 
   public add(property: string, value: number): Identify {
-    if (this._validate(IdentifyOperation.ADD, property, value)) {
-      this._safeSet(IdentifyOperation.ADD, property, value);
-    }
-
+    this._safeSet(IdentifyOperation.ADD, property, value);
     return this;
   }
 
   public unset(property: string): Identify {
-    if (this._validate(IdentifyOperation.UNSET, property, UNSET_VALUE)) {
-      this._safeSet(IdentifyOperation.ADD, property, UNSET_VALUE);
-    }
-
+    this._safeSet(IdentifyOperation.ADD, property, UNSET_VALUE);
     return this;
   }
 
@@ -93,15 +80,17 @@ export class Identify {
     return this;
   }
 
-  private _safeSet(operation: IdentifyOperation, property: string, value: any = null): void {
-    let userPropertyMap: any = this._userProperties[operation];
-    if (!userPropertyMap) {
-      userPropertyMap = {};
-      this._userProperties[operation] = userPropertyMap;
-    }
+  private _safeSet(operation: IdentifyOperation, property: string, value: any): void {
+    if (this._validate(operation, property, value)) {
+      let userPropertyMap: any = this._userProperties[operation];
+      if (!userPropertyMap) {
+        userPropertyMap = {};
+        this._userProperties[operation] = userPropertyMap;
+      }
 
-    userPropertyMap[property] = value;
-    this._userPropertySet.add(property);
+      userPropertyMap[property] = value;
+      this._userPropertySet.add(property);
+    }
   }
 
   private _validate(operation: IdentifyOperation, property: string, value: any): boolean {
