84d5d736db95b19fddfb4868fb646e5be37208f2
Kelvin Lu <kellu1997@gmail.com>
2021-03-02 11:07:18 -0800

    feat(validation): tougher rules on validate

--- packages/utils/src/validate.ts
+++ packages/utils/src/validate.ts
@@ -7,8 +7,21 @@ export const isValidEvent = (event: Event): boolean => {
     return false;
   }
 
-  if (event.device_id === undefined && event.user_id === undefined) {
+  const hasDeviceId = event.device_id !== undefined;
+  const hasUserId = event.user_id !== undefined;
+
+  if (!hasDeviceId && !hasUserId) {
     logger.warn('Invalid event: expected at least one of device or user id');
+    return false;
+  }
+
+  if (hasDeviceId && typeof event.device_id !== 'string') {
+    logger.warn('Invalid event: expected device id to be a string if present');
+    return false;
+  }
+
+  if (hasUserId && typeof event.user_id !== 'string') {
+    logger.warn('Invalid event: expected user id to be a string if present');
     return false;
   }
 
