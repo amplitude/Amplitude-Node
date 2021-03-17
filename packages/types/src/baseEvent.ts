c3870ad8429f17b43bfd9af3a38b29b0883457ff
AJ Horst <AJ_Horst@intuit.com>
2021-03-17 14:53:50 -0700

    fix(event): add similar warning to location fields

--- packages/types/src/baseEvent.ts
+++ packages/types/src/baseEvent.ts
@@ -19,9 +19,6 @@ export interface BaseEvent {
 
   // Optional
   time?: number;
-  country?: string;
-  region?: string;
-  city?: string;
   location_lat?: number;
   location_lng?: number;
 
@@ -30,8 +27,9 @@ export interface BaseEvent {
   library?: string;
 
   /** Optional
-   * Warning: updating any one of these fields will reset the other fields to null on the backend, unless the
-   * other fields are also set. See https://developers.amplitude.com/docs/http-api-v2 (Footnote 2) for more info
+   * Warning: updating any one of the following seven fields will reset the other fields
+   * to null on the backend, unless the other fields are also set.
+   * See https://developers.amplitude.com/docs/http-api-v2 (Footnote 2) for more info
    */
   platform?: string;
   os_name?: string;
@@ -41,13 +39,21 @@ export interface BaseEvent {
   device_model?: string;
   carrier?: string;
 
+  /** Optional
+   * Warning: updating any one of the following four fields will reset the other fields
+   * to null on the backend, unless the other fields are also set.
+   * See https://developers.amplitude.com/docs/http-api-v2 (Footnote 3) for more info
+   */
+  country?: string;
+  region?: string;
+  city?: string;
+  dma?: string; // ** The current Designated Market Area of the user. */
+
   idfa?: string;
   idfv?: string;
   adid?: string;
   android_id?: string;
 
-  // ** The current Designated Market Area of the user. */
-  dma?: string;
   language?: string;
   ip?: string;
   uuid?: string;
