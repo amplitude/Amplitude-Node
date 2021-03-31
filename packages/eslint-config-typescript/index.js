14adecf83f5266b0ab13529462376ded42211707
Kelvin Lu <kellu1997@gmail.com>
2021-03-31 14:02:59 -0700

    build: Reduce # of times packages get built, better specify tsconfig (#97)
    * ci(build): only build the packages once
    
    * fix(config): specify dirname, also add back
    
    * better test names

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -17,9 +17,10 @@ module.exports = {
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json',
     ecmaVersion: 2018,
+    project: '../../tsconfig.json',
     sourceType: 'module',
+    tsconfigRootDir: __dirname,
   },
   plugins: ['@typescript-eslint'],
   rules: {
