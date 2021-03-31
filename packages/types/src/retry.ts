9d2a9770d55d2309f1cc1954c44fd9ef2beeb60c
Kelvin Lu <kellu1997@gmail.com>
2021-03-30 17:51:16 -0700

    fix(retry): BaseRetryHandler + Make Retry in general easier to play with (#88)
    * refactor: base retry handler
    
    * fix: add option loading into the offline retrier
    
    * fix: dont throw, for consistency
    
    * dont use deprecated names
    
    * docs: add retry docs

--- packages/types/src/retry.ts
+++ packages/types/src/retry.ts
@@ -2,7 +2,7 @@ import { Event } from './event';
 import { Response } from './response';
 
 /** Layer used to send data to Amplitude while retrying throttled events in the right order.  */
-export interface RetryClass {
+export interface Retry {
   /**
    * Send the events payload to Amplitude, and retry the events that failed on a loop.
    *
@@ -10,3 +10,8 @@ export interface RetryClass {
    */
   sendEventsWithRetry(events: readonly Event[]): Promise<Response>;
 }
+
+/**
+ * @deprecated a poor naming of an interface that should be implemented, NOT a class to extend.
+ */
+export type RetryClass = Retry;
