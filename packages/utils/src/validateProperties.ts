d6aa203089f7b174c841f8499b2336b171a5e78a
Aaron Kosel <aaron@withcompound.com>
2021-09-28 09:27:48 -0400

    Fix parentheses ordering

--- packages/utils/src/validateProperties.ts
+++ packages/utils/src/validateProperties.ts
@@ -35,7 +35,7 @@ const isValidProperties = (property: string, value: any): boolean => {
     }
   } else if (typeof value === 'object') {
     return _isValidObject(value);
-  } else if (!(typeof value === 'number' || typeof value === 'string') || typeof value === 'boolean') {
+  } else if (!(typeof value === 'number' || typeof value === 'string' || typeof value === 'boolean')) {
     logger.warn('invalid value type ', typeof value);
     return false;
   }
