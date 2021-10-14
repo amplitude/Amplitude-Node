8cc894f05425b8eb49e564e8bba6250abc13667e
justin.fiedler <justin.fiedler@amplitude.com>
2021-10-14 04:58:08 +0000

    chore: lint fix

--- packages/node/test/middleware.test.ts
+++ packages/node/test/middleware.test.ts
@@ -1,5 +1,5 @@
-import { MiddlewareRunner } from "../src/middleware/middlewareRunner";
-import { MiddlewarePayload } from "@amplitude/types";
+import { MiddlewareRunner } from '../src/middleware/middlewareRunner';
+import { MiddlewarePayload } from '@amplitude/types';
 
 const PAYLOAD_EVENT_ONLY = { event: { event_type: 'Test ' } };
 
@@ -13,7 +13,7 @@ describe('middleware behavior', () => {
   });
 
   it('should call next() with no middleware', () => {
-    runner.run(PAYLOAD_EVENT_ONLY, next)
+    runner.run(PAYLOAD_EVENT_ONLY, next);
 
     expect(next).toHaveBeenCalledTimes(1);
     expect(next).toHaveBeenCalledWith(PAYLOAD_EVENT_ONLY);
@@ -25,7 +25,7 @@ describe('middleware behavior', () => {
     });
 
     runner.add(middleware);
-    runner.run(PAYLOAD_EVENT_ONLY, next)
+    runner.run(PAYLOAD_EVENT_ONLY, next);
 
     expect(middleware).toHaveBeenCalledTimes(1);
     expect(middleware.mock.calls[0][0]).toEqual(PAYLOAD_EVENT_ONLY);
@@ -39,7 +39,7 @@ describe('middleware behavior', () => {
     const middleware = jest.fn();
 
     runner.add(middleware);
-    runner.run(PAYLOAD_EVENT_ONLY, next)
+    runner.run(PAYLOAD_EVENT_ONLY, next);
 
     expect(middleware).toHaveBeenCalledTimes(1);
     expect(middleware.mock.calls[0][0]).toEqual(PAYLOAD_EVENT_ONLY);
@@ -52,33 +52,33 @@ describe('middleware behavior', () => {
     const middleware = [];
     const getPayloadWithCount = (count: number): MiddlewarePayload => ({
       event: { event_type: 'Test' },
-      extra: { count }
+      extra: { count },
     });
 
     // Create a number of middleware, each one increments the extra.count
-    for (let i = 0; i < middlewareCount; i +=1) {
+    for (let i = 0; i < middlewareCount; i += 1) {
       middleware[i] = jest.fn((payload, next) => {
         let { count = 0 } = { ...payload.extra };
-        count = +(count) + 1;
-        next({ ...payload, extra: { count }});
+        count = +count + 1;
+        next({ ...payload, extra: { count } });
       });
     }
 
     // Add middleware to run
-    for(let i = 0; i < middlewareCount; i += 1) {
+    for (let i = 0; i < middlewareCount; i += 1) {
       runner.add(middleware[i]);
     }
     // Run with payload with count = 0
     runner.run(getPayloadWithCount(0), next);
 
     // Check all middleware was called
-    for(let i = 0; i < middlewareCount; i += 1) {
+    for (let i = 0; i < middlewareCount; i += 1) {
       expect(middleware[i]).toHaveBeenCalledTimes(1);
       expect(middleware[i].mock.calls[0][0]).toEqual(getPayloadWithCount(i));
     }
 
     // Check call order
-    for(let i = 0; i < middlewareCount; i += 1) {
+    for (let i = 0; i < middlewareCount; i += 1) {
       const nextMethod = i < middlewareCount - 1 ? middleware[i + 1] : next;
       expect(middleware[i].mock.invocationCallOrder[0]).toBeLessThan(nextMethod.mock.invocationCallOrder[0]);
     }
